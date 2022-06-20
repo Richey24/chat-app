40abd9880f8cb2d3e0e24b399dce9defc1fc8769
Rejoice <uahomorejoice@gmail.com>
2022-06-21 00:08:47 +0100

    lot of changes

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,20 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { 
-    cors: {
-        origin: "http://127.0.0.1:5500"
-      }
- });
-const port = process.env.PORT || 5000
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+const port = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
-  socket.on("newMessage", (message)=> {
-    socket.broadcast.emit("message",message)
-  })
+  socket.on("newMessage", (message, userId) => {
+    socket.to(userId).emit("message", message);
+  });
+  socket.on("join-room", (userId) => {
+    socket.join(userId);
+  });
 });
 
 httpServer.listen(port);
--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,87 @@
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:5000", { autoConnect: false });
 const left = document.getElementById("left");
 const heading = document.getElementById("heading");
 const message = document.getElementById("message");
+
+const myForm = document.getElementById("form");
+myForm !== null && (myForm.onsubmit = submit);
+
+async function submit(event) {
+  event.preventDefault();
+  const user = {
+    username: event.target.username.value,
+    password: event.target.password.value,
+  };
+  const rep = await fetch("https://dreamtechhotel.herokuapp.com/user/login", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  const res = await rep.json();
+  localStorage.setItem("userId", res._id);
+  localStorage.setItem("username", res.username);
+  if (res.role === "Customer") {
+    window.location.href = "http://127.0.0.1:5500/frontend/customer.html";
+  } else {
+    window.location.href = "http://127.0.0.1:5500/frontend/admin.html";
+  }
+  console.log(res);
+}
+
+function connect() {
+  let userId = localStorage.getItem("userId");
+  if (!userId) {
+    window.location.href = "http://127.0.0.1:5500/frontend/login.html";
+  }
+  socket.connect();
+}
+
+function check() {
+  let userId = localStorage.getItem("userId");
+  if (userId) {
+    window.location.href = "http://127.0.0.1:5500/frontend/customer.html";
+  }
+}
+
 socket.on("connect", () => {
-  heading.innerHTML = "you connected with id " + socket.id;
+  let userId = localStorage.getItem("userId");
+  let username = localStorage.getItem("username");
+  console.log(userId, username);
+  socket.emit("join-room", userId);
+  heading.innerHTML = `Welcome ${username} to the best hotel in the city, Feel free to request for any services`;
 });
 
 socket.on("message", (message) => {
   const recievedMessage = document.createElement("p");
-  const breakLine = document.createElement("br");
-  recievedMessage.className = "left";
+  const container = document.createElement("div");
   recievedMessage.innerHTML = message;
-  if (left.lastElementChild?.className === "left") {
-    left.lastElementChild.innerHTML = left.lastElementChild.innerHTML + breakLine.outerHTML + message;
+  recievedMessage.className = "left";
+  container.innerHTML = recievedMessage.outerHTML;
+  container.className = "recievedDiv";
+  if (left.lastElementChild?.className === "recievedDiv") {
+    left.lastElementChild.appendChild(recievedMessage);
     return;
   }
-  left.appendChild(recievedMessage);
+  left.appendChild(container);
 });
 
 function send() {
-    if(message.value === "") return
-  socket.emit("newMessage", message.value);
+  if (message.value === "") return;
+  let userId = localStorage.getItem("userId");
+  socket.emit("newMessage", message.value, userId);
   const yourMessage = document.createElement("p");
-  const breakLine = document.createElement("br");
-  yourMessage.className = "right";
+  const container = document.createElement("div");
   yourMessage.innerHTML = message.value;
-  if (left.lastElementChild?.className === "right") {
-    left.lastElementChild.innerHTML =
-      left.lastElementChild.innerHTML + breakLine.outerHTML + message.value;
+  yourMessage.className = "right";
+  container.innerHTML = yourMessage.outerHTML;
+  container.className = "sendDiv";
+  if (left.lastElementChild?.className === "sendDiv") {
+    left.lastElementChild.appendChild(yourMessage);
     message.value = "";
     return;
   }
   message.value = "";
-  left.appendChild(yourMessage);
+  left.appendChild(container);
 }
