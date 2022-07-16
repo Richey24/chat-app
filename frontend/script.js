bd69e5d888227c469a999d9b724b070222ef77d6
Rejoice <uahomorejoice@gmail.com>
2022-07-16 02:15:54 +0100

    changes

---
+++ frontend/script.js
@@ -0,0 +1,111 @@
+const socket = io("http://localhost:5000", { autoConnect: false });
+const left = document.getElementById("left");
+const heading = document.getElementById("heading");
+const message = document.getElementById("message");
+const customerList = document.getElementById("customerList");
+const customerName = document.getElementById("customerName");
+
+const myForm = document.getElementById("form");
+myForm !== null && (myForm.onsubmit = submit);
+
+async function submit(event) {
+  event.preventDefault();
+  const user = {
+    username: event.target.username.value,
+    password: event.target.password.value,
+  };
+  const rep = await fetch("https://dreamtechhotel.herokuapp.com/user/login", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  const res = await rep.json();
+  localStorage.setItem("userId", res._id);
+  localStorage.setItem("username", res.username);
+  localStorage.setItem("role", res.role);
+  if (res.role === "Customer") {
+    window.location.href = "http://127.0.0.1:5500/frontend/customer.html";
+  } else {
+    window.location.href = "http://127.0.0.1:5500/frontend/admin.html";
+  }
+  console.log(res);
+}
+
+function connect() {
+  let userId = localStorage.getItem("userId");
+  if (!userId) {
+    window.location.href = "http://127.0.0.1:5500/frontend/login.html";
+  }
+  socket.connect();
+}
+
+function check() {
+  let userId = localStorage.getItem("userId");
+  let role = localStorage.getItem("role");
+  if (userId) {
+    role === "Customer" &&
+      (window.location.href = "http://127.0.0.1:5500/frontend/customer.html");
+    role === "Employee" &&
+      (window.location.href = "http://127.0.0.1:5500/frontend/admin.html");
+  }
+}
+
+socket.on("connect", () => {
+  let userId = localStorage.getItem("userId");
+  let username = localStorage.getItem("username");
+  socket.emit("join-room", userId);
+  socket.emit("get-message", userId);
+  socket.emit("link", "34847584984");
+  heading.innerHTML = `Welcome ${username} to the best hotel in the city, Feel free to request for any services`;
+});
+
+socket.on("message1", (messages) => {
+  let username = localStorage.getItem("username");
+  if (messages === null) return;
+  messages.map((mes) => {
+    const temp = document.createElement("template");
+    let mess = mes.trim();
+    temp.innerHTML = mess;
+    if (
+      temp.content.firstElementChild.lastElementChild.innerHTML === username
+    ) {
+      temp.content.firstElementChild.lastElementChild.innerHTML = "You";
+    }
+    left.appendChild(temp.content.firstChild);
+    left.lastElementChild.scrollIntoView();
+  });
+});
+
+socket.on("message", (messages) => {
+  let username = localStorage.getItem("username");
+  const temp = document.createElement("template");
+  let mes = messages[messages.length - 1].trim();
+  temp.innerHTML = mes;
+
+  if (temp.content.firstElementChild.lastElementChild.innerHTML === username) {
+    temp.content.firstElementChild.lastElementChild.innerHTML = "You";
+  }
+  left.appendChild(temp.content.firstChild);
+  left.lastElementChild.scrollIntoView();
+});
+
+function send() {
+  if (message.value === "") return;
+  let userId = localStorage.getItem("userId");
+  let username = localStorage.getItem("username");
+  const recievedMessage = document.createElement("p");
+  const yourName = document.createElement("span");
+  yourName.className = "yourName";
+  yourName.innerHTML = username;
+  const contain = document.createElement("div");
+  recievedMessage.innerHTML = message.value;
+  recievedMessage.className = "right";
+  contain.innerHTML = recievedMessage.outerHTML;
+  contain.appendChild(yourName);
+  contain.className = "recievedDiv";
+  socket.emit("newMessage", contain.outerHTML, userId);
+  socket.emit("noti", "34847584984", username);
+  message.value = "";
+}
---
+++ frontend/second.js
@@ -0,0 +1,106 @@
+const socket = io("http://localhost:5000");
+const left = document.getElementById("left");
+const heading = document.getElementById("heading");
+const message = document.getElementById("message");
+const customerList = document.getElementById("customerList");
+const customerName = document.getElementById("customerName");
+const noti = document.getElementById("noti");
+
+let id = "";
+
+socket.on("connect", () => {
+  socket.emit("link", "34847584984");
+});
+
+socket.on("message", (messages, secondId) => {
+  if (id !== secondId) return;
+  const temp = document.createElement("template");
+  let mes = messages[messages.length - 1].trim();
+  temp.innerHTML = mes;
+  if (
+    temp.content.firstElementChild.lastElementChild.innerHTML ===
+    "Customer service"
+  ) {
+    temp.content.firstElementChild.lastElementChild.innerHTML = "You";
+  }
+  left.appendChild(temp.content.firstChild);
+  left.lastElementChild.scrollIntoView();
+});
+
+socket.on("message1", (messages) => {
+  if (messages === null) return;
+  messages.map((mes) => {
+    const temp = document.createElement("template");
+    let mess = mes.trim();
+    temp.innerHTML = mess;
+    if (
+      temp.content.firstElementChild.lastElementChild.innerHTML ===
+      "Customer service"
+    ) {
+      temp.content.firstElementChild.lastElementChild.innerHTML = "You";
+    }
+    left.appendChild(temp.content.firstChild);
+    left.lastElementChild.scrollIntoView();
+  });
+});
+
+socket.on("noti", (username) => {
+  noti.innerHTML = `New message from ${username}`;
+});
+
+function send() {
+  if (message.value === "") return;
+
+  const recievedMessage = document.createElement("p");
+  const yourName = document.createElement("span");
+  yourName.className = "yourName";
+  yourName.innerHTML = "Customer service";
+  const contain = document.createElement("div");
+  recievedMessage.innerHTML = message.value;
+  recievedMessage.className = "right";
+  contain.innerHTML = recievedMessage.outerHTML;
+  contain.appendChild(yourName);
+  contain.className = "recievedDiv";
+
+  socket.emit("newMessage", contain.outerHTML, id);
+
+  message.value = "";
+  noti.innerHTML = "";
+}
+
+async function getAllUser() {
+  let userId = localStorage.getItem("userId");
+  let role = localStorage.getItem("role");
+
+  if (!userId || role !== "Employee") {
+    window.location.href = "http://127.0.0.1:5500/frontend/login.html";
+    return;
+  }
+  let response = await fetch(
+    "https://dreamtechhotel.herokuapp.com/user/get/all"
+  );
+  let users = await response.json();
+  let customers = users.user.filter((user) => user.role === "Customer");
+  customers.forEach((customer) => {
+    let singleCus = document.createElement("li");
+    let chat = document.createElement("p");
+    chat.id = customer._id;
+    singleCus.innerHTML = customer.username;
+    chat.innerHTML = "Chat";
+    singleCus.appendChild(chat);
+    customerList.appendChild(singleCus);
+  });
+
+  customers.forEach((single) => {
+    let oneUser = document.getElementById(single._id);
+    oneUser.addEventListener("click", () => {
+      document.getElementById("chatDiv").style.display = "block";
+      document.getElementById("chatDiv").scrollIntoView({ behavior: "smooth" });
+      left.innerHTML = "";
+      customerName.innerHTML = `You are in a chat with ${single.username}`;
+      id = single._id;
+      socket.emit("join-room", single._id);
+      socket.emit("get-message", single._id);
+    });
+  });
+}
