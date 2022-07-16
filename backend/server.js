c6db243938cf864cffd33aebd95531eef3a6bf7e
Rejoice <uahomorejoice@gmail.com>
2022-07-16 02:58:16 +0100

    backend chat app

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://brave-ocean-0db6b1310.1.azurestaticapps.net",
+  },
+});
+const port = process.env.PORT || 5000;
+const allMessage = {};
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", (message, userId) => {
+    if (userId in allMessage) {
+      allMessage[userId].push(message);
+    } else {
+      allMessage[userId] = [];
+      allMessage[userId].push(message);
+    }
+    io.to(userId).emit("message", allMessage[userId], userId);
+  });
+  socket.on("noti", (link, username) => {
+    socket.to(link).emit("noti", username);
+  });
+  socket.on("link", (link) => {
+    socket.join(link);
+  });
+  socket.on("get-message", (userId) => {
+    socket.emit("message1", allMessage[userId]);
+  });
+  socket.on("join-room", (userId) => {
+    socket.join(userId);
+  });
+});
+
+httpServer.listen(port);
